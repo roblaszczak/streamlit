ef3535f085438fe784d0f364b202be9666c4fb77
Kamil Bregula <kamil.bregula@snowflake.com>
2022-08-17 23:21:58 +0200

    Update license header

--- e2e/run_compose.py
+++ e2e/run_compose.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2022 Snowflake Inc.
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- lib/bin/streamlit.cmd
+++ lib/bin/streamlit.cmd
@@ -1,4 +1,4 @@
-Rem  Copyright 2022 Snowflake Inc.
+Rem  Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
 Rem
 Rem  Licensed under the Apache License, Version 2.0 (the "License");
 Rem  you may not use this file except in compliance with the License.
